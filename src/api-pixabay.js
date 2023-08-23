441b5a65aac131dc0215f060ae61fcbc623f8d14
LiudmylaHorobets <horobets1323@gmail.com>
2023-08-23 14:00:56 +0300

    add api pixabay

---
+++ src/api-pixabay.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '38912022-909e19910279754e6bef7369e';
+
+async function getImage(q, page) {
+  const searchParams = new URLSearchParams({
+    key: API_KEY,
+    q: q,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: 'true',
+    page: page,
+    per_page: 40,
+  });
+
+  const response = await axios.get(`${BASE_URL}?${searchParams}`);
+  const data = await response.data;
+  return data;
+}
+export { getImage };
