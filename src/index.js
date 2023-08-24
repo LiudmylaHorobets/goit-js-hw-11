c0be06d532bd75be39c361326fd88ea0280a2646
LiudmylaHorobets <horobets1323@gmail.com>
2023-08-24 20:26:34 +0300

    fixed bugs

--- src/index.js
+++ src/index.js
@@ -57,22 +57,36 @@ loadMoreBtn.addEventListener('click', async () => {
 //     const response = await getImage(q, page);
 //     const images = response.hits;
 
-//     if (images && images.length > 0) {
+//     if (images.length > 0) {
 //       createImageMarkup(images, gallery, lightbox);
+
+//       // Плавне прокручування після запиту
+//       //  const { height: cardHeight } =
+//       //    gallery.firstElementChild.getBoundingClientRect();
+//       //  window.scrollBy({
+//       //    top: cardHeight * images.length,
+//       //    behavior: 'smooth',
+//       //  });
 //     } else {
 //       loadMoreBtn.style.display = 'none';
-//       gallery.innerHTML =
-//         '<p>Sorry, there are no images matching your search query. Please try again.</p>';
+//       //   gallery.innerHTML =
+//       //     '<p>Sorry, there are no images matching your search query. Please try again.</p>';
+//       Notify.info(
+//         'Sorry, there are no images matching your search query. Please try again.'
+//       );
 //     }
 
 //     if (currentPage < Math.ceil(response.totalHits / 40)) {
 //       loadMoreBtn.style.display = 'block';
 //     } else {
-//       loadMoreBtn.style.display = 'none';
-//       gallery.insertAdjacentHTML(
-//         'beforeend',
-//         "<p>We're sorry, but you've reached the end of search results.</p>"
-//       );
+//         loadMoreBtn.style.display = 'none';
+//         // повідомлення про кінець images
+//        if (images.length === 0 && response.totalHits > 0) {
+//          gallery.insertAdjacentHTML(
+//            'beforeend',
+//            "<p>We're sorry, but you've reached the end of search results.</p>"
+//          );
+//        }
 //     }
 //   } catch (error) {
 //     console.error('Error fetching images:', error);
@@ -82,41 +96,48 @@ loadMoreBtn.addEventListener('click', async () => {
 //     throw new Error('Error fetching images from Pixabay API');
 //   }
 // }
+
+// ========================== 4
+let allImages = [];
+let totalPages = 0;
+
 async function fetchAndDisplayImages(q, page) {
   try {
     const response = await getImage(q, page);
     const images = response.hits;
 
+    if (page === 1) {
+      allImages = images;
+      totalPages = Math.ceil(response.totalHits / 40);
+      gallery.innerHTML = '';
+    } else {
+      allImages = [...allImages, ...images];
+    }
+
     if (images.length > 0) {
       createImageMarkup(images, gallery, lightbox);
 
-      // Плавне прокручування після запиту
-      //  const { height: cardHeight } =
-      //    gallery.firstElementChild.getBoundingClientRect();
-      //  window.scrollBy({
-      //    top: cardHeight * images.length,
-      //    behavior: 'smooth',
-      //  });
+      if (page < totalPages) {
+        loadMoreBtn.style.display = 'block';
+      } else {
+        loadMoreBtn.style.display = 'none';
+        if (allImages.length > 0) {
+          gallery.insertAdjacentHTML(
+            'beforeend',
+            "<p class='end-of-results-message'>We're sorry, but you've reached the end of search results.</p>"
+          );
+        }
+      }
     } else {
       loadMoreBtn.style.display = 'none';
-      //   gallery.innerHTML =
-      //     '<p>Sorry, there are no images matching your search query. Please try again.</p>';
-      Notify.info(
-        'Sorry, there are no images matching your search query. Please try again.'
-      );
-    }
-
-    if (currentPage < Math.ceil(response.totalHits / 40)) {
-      loadMoreBtn.style.display = 'block';
-    } else {
-        loadMoreBtn.style.display = 'none';
-        // повідомлення про кінець images
-       if (images.length === 0 && response.totalHits > 0) {
-         gallery.insertAdjacentHTML(
-           'beforeend',
-           "<p>We're sorry, but you've reached the end of search results.</p>"
-         );
-       }
+      if (allImages.length === 0) {
+        gallery.insertAdjacentHTML('beforeend');
+      } else {
+        gallery.insertAdjacentHTML(
+          'beforeend',
+          "<p class='end-of-results-message'>We're sorry, but you've reached the end of search results.</p>"
+        );
+      }
     }
   } catch (error) {
     console.error('Error fetching images:', error);
